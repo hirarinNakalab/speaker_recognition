02e45057d8b15e953a60b7436102fa50d69ea413
nakaLab <hirakawa.rin151@mail.kyutech.jp>
2020-01-04 21:44:53 +0900

    optimize refactored

--- helpers.py
+++ helpers.py
@@ -7,7 +7,7 @@ from sklearn.metrics import f1_score, confusion_matrix
 import os
 import random
 import pysptk
-import torchaudio
+import soundfile as sf
 import numpy as np
 import pyworld as pw
 
@@ -34,10 +34,15 @@ def get_features(x, fs):
     bap = pw.code_aperiodicity(ap, fs)
     return f0, mcep, bap
 
-def normalize(tensor):
-    # Subtract the mean, and scale to the interval [-1,1]
-    tensor_minusmean = tensor - tensor.mean()
-    return tensor_minusmean/tensor_minusmean.abs().max()
+def normalize(data):
+    data = data.astype(np.float64)
+    amp = max(np.abs(np.max(data)), np.abs(np.min(data)))
+    data = data / amp
+    data.clip(-1, 1)
+    return data
+
+def sort_dict(dict):
+    return sorted(dict.items(), key=lambda x: x[1])
 
 def sort_dict_reverse(dict):
     return sorted(dict.items(), key=lambda x: x[1], reverse=True)
@@ -61,12 +66,12 @@ class Experiment:
     def execute(self, data, model):
         print("wavefile:{}".format(os.path.basename(data)))
 
-        x, fs = torchaudio.load(data)
-        x = normalize(x).numpy().reshape(-1).astype(np.float64)
+        x, fs = sf.read(data)
+        x = normalize(x)
 
         f0, mcep, bap = get_features(x, fs)
         features = np.concatenate([
-            f0.reshape[:, np.newaxis],
+            f0.reshape(-1, 1),
             mcep[:, :self.n_features - 2],
             -bap
         ], axis=1)
@@ -78,7 +83,6 @@ class Experiment:
 
         print("average anomaly score:", np.mean(anomaly), end='\n\n')
         return np.mean(anomaly)
-
 
 class OVRClassifier:
     def __init__(self, models, sp2idx, experiment):
@@ -95,21 +99,31 @@ class OVRClassifier:
         return ans
 
     def optimize(self, train_data):
-        all_anoms = set()
-        for model in self.models.values():
-            model.eval()
-            for data in train_data:
-                all_anoms.add(self.exp.execute(data, model))
+        all_anoms = defaultdict(lambda: defaultdict(float))
+        for data in train_data:
+            for model_name, model in self.models.items():
+                model.eval()
+                all_anoms[data][model_name] = self.exp.execute(data, model)
+
+        anom_patterns = {all_anoms[data][model_name]
+                         for data in train_data
+                         for model_name in self.models.keys()}
 
         results = defaultdict(float)
-        for th in sorted(all_anoms, reverse=True):
+        for th in sorted(anom_patterns, reverse=True):
             self.threshold = th
             ans = [self.get_speaker_idx(data) for data in train_data]
-            pred = [self.predict(data) for data in train_data]
+            pred = []
+            for data in train_data:
+                anoms = all_anoms[data]
+                anom_sorted = sort_dict(anoms)
+                is_over_th = [(val > self.threshold) for val in anoms.values()]
+                pred_sp = "unk" if all(is_over_th) else anom_sorted[0][0]
+                pred.append(pred_sp)
             results[th] = f1_score(ans, pred)
 
         results_sorted = sort_dict_reverse(results)
-        self.threshold = float(results_sorted[0][1])
+        self.threshold = float(results_sorted[0][0])
 
     def predict(self, data):
         anomalies = {}
@@ -119,7 +133,7 @@ class OVRClassifier:
             model = self.models[speaker]
             model.eval()
             anomalies[speaker] = self.exp.execute(data, model)
-        anom_sorted = sort_dict_reverse(anomalies)
+        anom_sorted = sort_dict(anomalies)
 
         is_over_th = [(val > self.threshold) for val in anomalies.values()]
         pred_sp = "unk" if all(is_over_th) else anom_sorted[0][0]
@@ -131,7 +145,6 @@ class OVRClassifier:
         pred = [self.predict(data) for data in test_data]
         data_pair = (ans, pred)
         return f1_score(*data_pair), confusion_matrix(*data_pair)
-
 
 class Learner:
     def __init__(self, input_path, setting):
@@ -172,6 +185,8 @@ class Learner:
         test_dataset = defaultdict(list)
 
         for speaker in self.sp2idx.keys():
+            if speaker == "unk":
+                continue
             data = speakers_data[speaker]
             train_dataset[speaker] = data[:split_idx]
             test_dataset[speaker] = data[split_idx:min_length]
@@ -234,7 +249,7 @@ class Learner:
 
             for epoch in range(epoch):
                 print("epoch {}".format(epoch))
-                for data in random.shuffle(train_data):
+                for data in random.sample(train_data, len(train_data)):
                     self.experiment.execute(data, model)
 
             fmt = "training data count: {}"
--- recognition.py
+++ recognition.py
@@ -24,8 +24,8 @@ def main(args, default_parameters=param.default_parameters):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('-e', '--epoch', type=int, default=10,
-                        help='Number of epoch to learn, defaults to 10 epochs.')
+    parser.add_argument('-e', '--epoch', type=int, default=5,
+                        help='Number of epoch to learn, defaults to 5 epochs.')
     parser.add_argument('-s', '--speech', type=int, default=31,
                         help='The number of speech to learn, defaults to #31')
     args = parser.parse_args()
